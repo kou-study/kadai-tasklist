bf330cb2f42c6c7e8bc7134082cb31bcc84f5a2d
kou <ec2-user@ip-172-31-62-185.ec2.internal>
2020-07-28 09:27:28 +0000

    add db-seed

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
     def index
-        @tasks = Task.all.page(params[:page]).per(3)
+        @tasks = Task.all.page(params[:page]).per(10)
     end
     
     def show
