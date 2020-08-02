5eabba4cf61a3605581ecd0bc7ab021ac3ec6102
kou <ec2-user@ip-172-31-62-185.ec2.internal>
2020-08-02 15:48:33 +0000

    Modified

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
     before_action :require_user_logged_in
-    before_action :correct_user, only: [:show, :destroy, :update]
+    before_action :correct_user, only: [:show, :edit, :update, :destroy]
     
     def index
         @tasks = current_user.tasks.all.page(params[:page]).per(10)
